f20053c10b01bf7674d49966687ed4098de77340
Tuomas Hietanen <tuomas.hietanen@iki.fi>
2013-07-16 21:18:31 +0300

    css changes

--- wp-content/themes/mensafin/page-with-posts.php
+++ wp-content/themes/mensafin/page-with-posts.php
@@ -16,7 +16,7 @@ get_header(); ?>
 
 
 	<div id="primary" class="content-area">
-		<div id="content" class="site-content" role="main">
+		<div id="content" class="site-content" role="main" style="margin-right: 20px;">
 
 			<?php while ( have_posts() ) : the_post(); ?>
 
