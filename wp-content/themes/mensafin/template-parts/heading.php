5e3173a8d34ea4e10f47af4289ec03f19e978e97
kuffis <simo.harkonen@tintti.net>
2013-07-28 16:49:15 +0300

    Korva F T P Youtube -linkkilista Facebook Follow-napilla (Muita Some-linkkejä voi lisätä, jos pulju joskus aloitta Twitter/Youtube/Pinterest -tilejä.)

--- wp-content/themes/mensafin/template-parts/heading.php
+++ wp-content/themes/mensafin/template-parts/heading.php
@@ -9,11 +9,12 @@
 		<div id="MensaLogo"><a href="index.php"><img src="<?php bloginfo('template_directory'); ?>/images/MensaLogo.png" alt="Mensa Logo" /></a></div><!-- end MensaLogo -->
 		<div id="MensaText"><a href="index.php"><img src="<?php bloginfo('template_directory'); ?>/images/MensaText.png" alt="leiska" /></a></div><!-- end MensaText -->
 		<div id="network">
-			<div id="some"><img src="<?php bloginfo('template_directory'); ?>/images/some.png" width="190" height="64" alt="social media" />
-			</div><!-- end facebook -->
+		  <div id="some">
+			<div class="fb-follow" data-href="https://www.facebook.com/Mensantestit" data-layout="button_count" data-show-faces="false" data-width="250">
+			</div> <!-- Facebook -->
+		  </div> <!-- some -->
 		  <div id="members">
-		  
-		  
+
 		  <!-- hakuautomaatteja varten? t.ulla -->
 		  <h1>Mensa on kansainvälinen järjestö, jonka toiminnan tarkoituksena on älykkyyden tunnistaminen ja kehittäminen ihmisyyden hyväksi, älykkyystutkimuksen tukeminen sekä älyllisen ja sosiaalisen ympäristön tarjoaminen jäsenilleen. Mensan jäseneksi voi liittyä jokainen, joka saavuttaa älykkyystestissä paremman tuloksen kuin 98% väestöstä. mensa, älykkyystesti, mensan testi, älykkyys, äo, iq, äo testi, iq testi, testi, test, älykkyysosamäärä, aivokapasiteetti, nettitesti, kotitesti, soveltuvuustesti, suomen mensa, suomi, finland, älykkyystestit, pulmapeli, pulmapelit, sosiaalinen älykkyys, älypeli, älypää, psykologinen, logiikka, looginen, loogiset, päättely, kyky, tehtäviä, tunneäly, ilmainen nettitesti, numeerinen, numeeriset, raja, jakauma, online, järjestö, yhdistys</h1>
 		  
