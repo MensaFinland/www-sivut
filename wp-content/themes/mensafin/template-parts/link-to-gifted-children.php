91f4552276501a6ee6d27fb727e26feba927f475
kuffis <simo.harkonen@tintti.net>
2013-07-28 14:42:16 +0300

    Etusivun lahjakkaat lapset -linkki.

---
+++ wp-content/themes/mensafin/template-parts/link-to-gifted-children.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Front page link to "gifted children" -website.
+ *
+ * @package mensafin
+ */
+?>
+
+<a href="http://lahjakkaatlapset.fi">
+<img src="<?php bloginfo('template_directory') ?>/images/lahjakkaat-lapset-banneri.jpg"
+     alt="linkki lahjakkaat lapset -sivustolle"/>
+</a>
