e8bacc4fdb702f7d3efe1b3d671059b96ce1aeb3
Tuomas Hietanen <tuomas.hietanen@iki.fi>
2013-07-29 00:44:45 +0300

    testaan uutta some-kuvaa

--- wp-content/themes/mensafin/template-parts/heading.php
+++ wp-content/themes/mensafin/template-parts/heading.php
@@ -6,20 +6,25 @@
  */
 ?>
 	<header id="masthead" class="site-header" role="banner">
+		  <!-- hakuautomaatteja varten? t.ulla -->
+		  <h1>Mensa on kansainvälinen järjestö, jonka toiminnan tarkoituksena on älykkyyden tunnistaminen ja kehittäminen ihmisyyden hyväksi, älykkyystutkimuksen tukeminen sekä älyllisen ja sosiaalisen ympäristön tarjoaminen jäsenilleen. Mensan jäseneksi voi liittyä jokainen, joka saavuttaa älykkyystestissä paremman tuloksen kuin 98% väestöstä. mensa, älykkyystesti, mensan testi, älykkyys, äo, iq, äo testi, iq testi, testi, test, älykkyysosamäärä, aivokapasiteetti, nettitesti, kotitesti, soveltuvuustesti, suomen mensa, suomi, finland, älykkyystestit, pulmapeli, pulmapelit, sosiaalinen älykkyys, älypeli, älypää, psykologinen, logiikka, looginen, loogiset, päättely, kyky, tehtäviä, tunneäly, ilmainen nettitesti, numeerinen, numeeriset, raja, jakauma, online, järjestö, yhdistys</h1>
+	
 		<div id="MensaLogo"><a href="index.php"><img src="<?php bloginfo('template_directory'); ?>/images/MensaLogo.png" alt="Mensa Logo" /></a></div><!-- end MensaLogo -->
 		<div id="MensaText"><a href="index.php"><img src="<?php bloginfo('template_directory'); ?>/images/MensaText.png" alt="leiska" /></a></div><!-- end MensaText -->
 		<div id="network">
 		  <div id="some">
-			<div class="fb-follow" data-href="https://www.facebook.com/Mensantestit" data-layout="button_count" data-show-faces="false" data-width="250">
-			</div> <!-- Facebook -->
+			<img name="some" src="<?php bloginfo('template_directory'); ?>/images/networkKaikki.png" width="351" height="25" border="0" usemap="#some1">
+
+			<map name="some1">
+				<area shape="rect" coords="0,0,24,25" href="https://www.facebook.com/Mensantestit" target="_blank" class="fb-follow" alt="Mensan älykkyystestit Facebookissa">
+				<area shape="rect" coords="29,0,67,25" href="https://twitter.com/intent/follow?screen_name=mensafinland&tw_p=followbutton&variant=2.0" target="_blank" alt="Seuraa käyttäjää @MensaFinland">
+				<area shape="rect" coords="67,0,130,25" href="http://www.youtube.com" target="_blank" alt="Virallinen tili vasta tulossa...">
+				<area shape="rect" coords="134,0,169,25" href="http://pinterest.com/" target="_blank" alt="Virallinen Pinterst-tili...">
+				<area shape="rect" coords="209,0,351,25" href="https://www.mensa.fi/forum/" target="_blank" alt="Jäsenten sivusto">
+			</map>
 		  </div> <!-- some -->
 		  <div id="members">
-
-		  <!-- hakuautomaatteja varten? t.ulla -->
-		  <h1>Mensa on kansainvälinen järjestö, jonka toiminnan tarkoituksena on älykkyyden tunnistaminen ja kehittäminen ihmisyyden hyväksi, älykkyystutkimuksen tukeminen sekä älyllisen ja sosiaalisen ympäristön tarjoaminen jäsenilleen. Mensan jäseneksi voi liittyä jokainen, joka saavuttaa älykkyystestissä paremman tuloksen kuin 98% väestöstä. mensa, älykkyystesti, mensan testi, älykkyys, äo, iq, äo testi, iq testi, testi, test, älykkyysosamäärä, aivokapasiteetti, nettitesti, kotitesti, soveltuvuustesti, suomen mensa, suomi, finland, älykkyystestit, pulmapeli, pulmapelit, sosiaalinen älykkyys, älypeli, älypää, psykologinen, logiikka, looginen, loogiset, päättely, kyky, tehtäviä, tunneäly, ilmainen nettitesti, numeerinen, numeeriset, raja, jakauma, online, järjestö, yhdistys</h1>
 		  
-		  
-		  <a href="https://www.mensa.fi/forum/"><img id="forumLink" src="<?php bloginfo('template_directory'); ?>/images/jasen.png" width="180" height="64" alt="members area" style="cursor:pointer;" /></a>
 			</div><!-- end members -->
 	  </div><!-- end network --> 
 		
