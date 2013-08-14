fecfaf23941684dec3c6e402b0f0493fc3de04d2
kuffis <simo.harkonen@tintti.net>
2013-08-14 17:17:59 +0300

    Arkistosivu (esim. yhden kirjoittajan postaukset)

--- wp-content/themes/mensafin/template-parts/default-content-area.php
+++ wp-content/themes/mensafin/template-parts/default-content-area.php
@@ -8,14 +8,11 @@
  */
 ?>
 <div id="primary" class="content-area" style="width:555px; float:left; background-color:transparent;">
-<div id="content" class="site-content" role="main">
+  <div id="content" class="site-content" role="main">
+    <?php while ( have_posts() ) : the_post(); ?>
+      <?php global $content_style;
+            get_template_part( 'template-parts/content', $content_style ); ?>
 
-  <?php the_post(); //while ( have_posts() ) : the_post(); ?>
-
-    <?php global $content_style;
-          get_template_part( 'template-parts/content', $content_style ); ?>
-
-    <?php //endwhile; // end of the loop. ?>
-
+    <?php endwhile; // end of the loop. ?>
   </div><!-- #content -->
 </div><!-- #primary -->
