5592a6a92f2e1a2116e0433448284964f4de80fe
Tuomas Hietanen <tuomas.hietanen@iki.fi>
2013-08-21 23:40:18 +0300

    POS-sivun jatkoa

--- wp-content/themes/mensafin/page-template-pos-fi.php
+++ wp-content/themes/mensafin/page-template-pos-fi.php
@@ -22,6 +22,7 @@ get_header(); ?>
 			<?php the_post(); //while ( have_posts() ) : the_post(); ?>
 
 				<?php //get_template_part( 'content', 'page' ); ?>
+				<?php $contentlike_template_part = 'template-parts/pos-list'; ?>
 				<?php get_template_part( 'template-parts/content-page' ); ?>
 
 			<?php //endwhile; // end of the loop. ?>
@@ -30,9 +31,12 @@ get_header(); ?>
 	</div><!-- #primary -->
 	<div id="sidebar" class="pos-map">
 		<?php get_template_part( 'template-parts/pos-map' ); ?>	
-		<img id="posOtsake" alt="Paikallisosastot" src="<?php bloginfo('template_directory'); ?>/images/poskartta/POSheadline.jpg" style="border:thin black none"/>
 		<div class="posLista">
-		<?php $contentlike_template_part = 'template-parts/pos-list'; ?>
+			<img id="posOtsake" alt="Paikallisosastot" src="<?php bloginfo('template_directory'); ?>/images/poskartta/POSheadline.jpg" style="border:thin black none"/>
+			
+			Kartan pitaisi hakea 
+			valitun POS:in tiedot 
+			tahan...
 		</div>
 	</div><!-- #sidebar (secondary) -->
 </div> <!-- #container -->
