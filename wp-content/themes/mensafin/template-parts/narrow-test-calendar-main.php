537d3182fc349b2c66b20041329306dbc949d8c9
Tuomas Hietanen <tuomas.hietanen@iki.fi>
2013-08-22 21:02:23 +0300

    Testikalenteri, käännetty aikajärjestys

--- wp-content/themes/mensafin/template-parts/narrow-test-calendar-main.php
+++ wp-content/themes/mensafin/template-parts/narrow-test-calendar-main.php
@@ -18,7 +18,7 @@ $DA = new Data_Access;
 $DA->openDatabase();
 
 $hideGone = "and (ISNULL(CONCAT(DateYear, '-', DateMonth, '-', DateDay)) or DateYear=0 or STR_TO_DATE(CONCAT(DateYear, '-', DateMonth, '-', DateDay),'%Y-%m-%d') >= NOW() ) ";
-$sql="select Id, Title, Visible, EventType, DateYear, DateMonth, DateDay, DateHour, DateMinute, City, StreetAddress, LocationDetails, EventDetails, Latitude, Longitude from EventCalendar where Visible=0 " . $hideGone . "order by STR_TO_DATE(CONCAT(DateYear, '-', DateMonth, '-', DateDay),'%Y-%m-%d') DESC, Id DESC";
+$sql="select Id, Title, Visible, EventType, DateYear, DateMonth, DateDay, DateHour, DateMinute, City, StreetAddress, LocationDetails, EventDetails, Latitude, Longitude from EventCalendar where Visible=0 " . $hideGone . "order by STR_TO_DATE(CONCAT(DateYear, '-', DateMonth, '-', DateDay),'%Y-%m-%d') ASC, Id DESC";
 $eventdata=$DA->getValues($sql);
 
    
@@ -31,7 +31,7 @@ $eventdata=$DA->getValues($sql);
                     print "<div class='event'><ul><li>";
                     if($row["EventType"]=="0" || $row["EventType"]==0){
                         print "<span class='aika'>".htmlspecialchars($row["City"]);
-                        print " ".htmlspecialchars($row["DateDay"]).htmlspecialchars($row["DateMonth"]).htmlspecialchars($row["DateYear"]);
+                        print " ".htmlspecialchars($row["DateDay"]).".".htmlspecialchars($row["DateMonth"]).".".htmlspecialchars($row["DateYear"]);
                         print "</span></li><li>klo ".htmlspecialchars($row["DateHour"]).".".htmlspecialchars($row["DateMinute"])." ".htmlspecialchars($row["Title"]);
                         print "</li><li>".htmlspecialchars($row["LocationDetails"]);
                         print "</li><li>".htmlspecialchars($row["StreetAddress"]);
