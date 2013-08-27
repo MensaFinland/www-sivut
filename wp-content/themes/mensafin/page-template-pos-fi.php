c3e5a3cb3e94d9a4df22e6caa021519fc0b8a426
kuffis <simo.harkonen@tintti.net>
2013-08-27 20:14:29 +0300

    Vain yksi paikallisosastolistaus, jossa nuorisojaosto ekana.

--- wp-content/themes/mensafin/page-template-pos-fi.php
+++ wp-content/themes/mensafin/page-template-pos-fi.php
@@ -21,13 +21,8 @@ get_header(); ?>
 	<div id="primary" class="content-area" style="width: 525px; float:left; background-color:transparent;">
 		<div id="content" class="site-content" role="main">
 
-			<?php the_post(); //while ( have_posts() ) : the_post(); ?>
-
-				<?php //get_template_part( 'content', 'page' ); ?>
-				<?php $contentlike_template_part = 'template-parts/pos-list'; ?>
-				<?php get_template_part( 'template-parts/content-page' ); ?>
-
-			<?php //endwhile; // end of the loop. ?>
+			<?php the_post(); ?>
+			<?php get_template_part( 'template-parts/content-page' ); ?>
 
 		</div><!-- #content -->
 	</div><!-- #primary -->
--- wp-content/themes/mensafin/template-parts/pos-box.php
+++ wp-content/themes/mensafin/template-parts/pos-box.php
@@ -14,12 +14,12 @@
 
     /* When POS map is clicked, this is the link we scroll to. Example: <a id="pos14box"></a> */
     print('<div class="posOsasto">');
-    if ($pos['map_id']) { print('<a id="' . $pos["map_id"] . 'box"></a>'); }
+    if ($pos['map_id']) { print('<a id="' . $pos["map_id"] . '"></a>'); }
 
     print('<h3 class="pos">' . $pos["name"] . '</h3>');
     print('<h4>Toimialue:</h4><p>' . $pos["province"] . '</p>');
     foreach($pos["people"] as $person) {
-      print('<h4>' . $person['title'] . '</h4>');
+      print('<h4>' . $person['title'] . ':</h4>');
       print('<p>' . $person['name'] . '<br/>' . $person['email'] . '<br/>' . $person['tel'] . '</p>');
     }
     print('</div>'); /* div.poschapter */
