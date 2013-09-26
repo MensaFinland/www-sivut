f7d7aad6a35fd4eab6e5969cbc2949de34140080
Tuomas Hietanen <tuomas.hietanen@iki.fi>
2013-09-26 19:54:23 +0300

    nettitesti omaan sivupohjaansa

---
+++ wp-content/themes/mensafin/page-template-nettest-fi.php
@@ -0,0 +1,33 @@
+<?php
+/*
+Template Name: Nettitesti (suomenkielinen)
+*/
+
+/**
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package mensaFin
+ */
+
+get_header(); ?>
+<div id="container">
+<div id="container-edge"></div>
+    <script>
+      function openPopup() {
+        window.open("http://www.mensa.no/olavtesten/fi/index_2.html",'hjemmetest','height=700,width=670,toolbar=no,directories=no,status=no,menubar=no,scrollbars=no,resizable=no'); 
+        return false;
+      } 
+    </script>
+  <?php $content_style = 'page'; ?>
+  <?php get_template_part( 'template-parts/default-content-area' ); ?>
+  <a href="#" onClick="openPopup();">Aloita nettitesti tästä.<br></a>
+  <?php get_template_part( 'template-parts/default-sidebar'      ); ?>
+</div> <!-- #container -->
+
+<?php get_template_part( 'template-parts/sitemap-footer' ); ?>
+<?php get_footer(); ?>
