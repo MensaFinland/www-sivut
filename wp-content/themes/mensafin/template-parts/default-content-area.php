1b04ec4a971e85a7f6fa79b203bec785e7eada54
Tuomas Hietanen <tuomas.hietanen@iki.fi>
2013-09-26 20:40:47 +0300

    pistelinja pois

--- wp-content/themes/mensafin/template-parts/default-content-area.php
+++ wp-content/themes/mensafin/template-parts/default-content-area.php
@@ -12,7 +12,6 @@
     <?php while ( have_posts() ) : the_post(); ?>
       <?php global $content_style;
             get_template_part( 'template-parts/content', $content_style ); ?>
-	  <div class="pistelinja"></div><!-- end pistelinja -->
     <?php endwhile; // end of the loop. ?>
 	<img src="<?php bloginfo('template_directory'); ?>/images/loppunelio.jpg" width="10" height="10" alt="end of story" />
   </div><!-- #content -->
