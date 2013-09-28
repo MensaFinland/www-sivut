b0e44c618c69830da1e6787bbcc4a36a07e08df3
Tuomas Hietanen <tuomas.hietanen@iki.fi>
2013-09-28 12:39:10 +0300

    lists: ul li

--- wp-content/themes/mensafin/template-parts/narrow-test-calendar-main.php
+++ wp-content/themes/mensafin/template-parts/narrow-test-calendar-main.php
@@ -28,21 +28,21 @@ $eventdata=$DA->getValues($sql);
           <?php
             if(isset($eventdata)){
                 while($row=mysqli_fetch_array($eventdata, MYSQL_ASSOC)){
-                    print "<div class='event'><ul><li>";
+                    print "<div class='event'>";
                     if($row["EventType"]=="0" || $row["EventType"]==0){										
                         print "<div id='testInfo".htmlspecialchars($row["Id"])."' title='&Auml;lykkyystesti' class='testInfoDialog'>";
-                        print "<span class='aika'>".htmlspecialchars($row["City"]);
+                        print "<ul><li><span class='aika'>".htmlspecialchars($row["City"]);
                         print " ".htmlspecialchars($row["DateDay"]).".".htmlspecialchars($row["DateMonth"]).".".htmlspecialchars($row["DateYear"]);
 						$parseMin = htmlspecialchars($row["DateMinute"]);
                         print "</span></li><li>klo ".htmlspecialchars($row["DateHour"]).".".(intval($parseMin)<10?"0".$parseMin:$parseMin)." ".htmlspecialchars($row["Title"]);
                         print "</li><li>".htmlspecialchars($row["LocationDetails"]);
                         print "</li><li>".htmlspecialchars($row["StreetAddress"]);
                         print "</li><li>".htmlspecialchars($row["EventDetails"]);
-                        print "</div>";
-                        print "<a href='#' onclick='$(\"#testinfo".$row["Id"]."\").dialog(\"open\");'><span class='aika'>".htmlspecialchars($row["City"]);
+                        print "</li></ul></div>";
+                        print "<ul><li><a href='#' onclick='$(\"#testinfo".$row["Id"]."\").dialog(\"open\");'><span class='aika'>".htmlspecialchars($row["City"]);
                         print " ".htmlspecialchars($row["DateDay"]).".".htmlspecialchars($row["DateMonth"]).".".htmlspecialchars($row["DateYear"])."</a>";
                     } else {
-                        print "".htmlspecialchars($row["Title"]);
+                        print "<ul><li>".htmlspecialchars($row["Title"]);
                         print "</li><li>".htmlspecialchars($row["EventDetails"]);
                     }
                     print "</li></ul></div><!-- end event -->";
