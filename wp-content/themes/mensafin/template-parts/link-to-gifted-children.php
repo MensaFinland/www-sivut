fea3a7ac393e6e7e42a6e81e72fd3e4515b60549
Tuomas Hietanen <tuomas.hietanen@iki.fi>
2013-09-29 13:04:47 +0300

    button test

--- wp-content/themes/mensafin/template-parts/link-to-gifted-children.php
+++ wp-content/themes/mensafin/template-parts/link-to-gifted-children.php
@@ -8,7 +8,7 @@
 <div id="lahjakkaat">
   
   <p class="textLahjakkaat">
-    Mensan Lahjakkaat Lapset -projekti: 
+    Mensan Lahjakkaat Lapset -ohjelma: 
     <a href="http://lahjakkaatlapset.fi" target="_blank">lue lis&auml;&auml; lasten ja nuorten lahjakkuudesta</a>
   </p> <!-- .textBlogi -->
 </div> <!-- #blog -->
