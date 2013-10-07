3e332da674cbefcd652ff038f388eba67c4c7d74
Tuomas Hietanen <tuomas.hietanen@iki.fi>
2013-10-07 22:21:34 +0300

    korjaus

--- wp-content/themes/mensafin/template-parts/heading.php
+++ wp-content/themes/mensafin/template-parts/heading.php
@@ -6,12 +6,11 @@
  */
 ?>
 	<header id="masthead" class="site-header" role="banner">
-		  <!-- hakuautomaatteja varten? t.ulla -->
-		  <h1>Mensa on kansainvälinen järjestö, jonka toiminnan tarkoituksena on älykkyyden tunnistaminen ja kehittäminen ihmisyyden hyväksi, älykkyystutkimuksen tukeminen sekä älyllisen ja sosiaalisen ympäristön tarjoaminen jäsenilleen. Mensan jäseneksi voi liittyä jokainen, joka saavuttaa älykkyystestissä paremman tuloksen kuin 98% väestöstä.</h1>
-		  <br/><br/> Hakusanoja:
-		  <h1>mensa, mensa testi, älykkyystesti, mensan testi, äo testi, mensan kotitesti, älykkyysosamäärä, äly, älykkyys, yli, huippu, älykäs, huippuälykäs, älykkäille, älykkäiden, äo, iq, äo testi, iq testi, testi, test, aivokapasiteetti, nettitesti, kotitesti, soveltuvuustesti, suomen mensa, suomi, finland, älykkyystestit, eurooppalainen älykkyystesti,
-		  psykologinen testi, sosiaalinen älykkyys, älypeli, älypää, psykologinen, logiikka, looginen, loogiset, päättely, kyky, tehtäviä, tunneäly, ilmainen nettitesti, matemaattinen, numeerinen, numeeriset, raja, jakauma, online, järjestö, yhdistys, yhteisö, ryhmä, seura, seuraa, verkosto, vertais, tapahtuma, tapahtumia, kokemuksia, taito, lahjakkuus,
-		  lahjakas, lapsi, tuki, erilaisuus, perspektiivi, harrastus, ystävä, ystäviä, kavereita, keskustelu, sananvapaus, chat, huumori, pulmapeli, pulmapelit, vapaaehtois, hyväntekeväisyys, parantaa, sukututkimus, tutustuminen, tekemistä, kohtaamisia, yhteistyö, oppiminen, oppia, mahdollisuus, kansainvälinen, maailmanlaajuinen, toiminta, toimintaa, älyä, oivallusta, oivaltaminen, oivaltaa, &auml;lykkyys, &auml:o, &auml;lykas, alykkyys, aly, alykas</h1>
+<!-- hakuautomaatteja varten? t.ulla -->
+<h1>Mensa on kansainvälinen järjestö, jonka toiminnan tarkoituksena on älykkyyden tunnistaminen ja kehittäminen ihmisyyden hyväksi, älykkyystutkimuksen tukeminen sekä älyllisen ja sosiaalisen ympäristön tarjoaminen jäsenilleen. Mensan jäseneksi voi liittyä jokainen, joka saavuttaa älykkyystestissä paremman tuloksen kuin 98% väestöstä.</h1>
+<h1>Hakusanoja: mensa, mensa testi, älykkyystesti, mensan testi, äo testi, mensan kotitesti, älykkyysosamäärä, äly, älykkyys, yli, huippu, älykäs, huippuälykäs, älykkäille, älykkäiden, äo, iq, äo testi, iq testi, testi, test, aivokapasiteetti, nettitesti, kotitesti, soveltuvuustesti, suomen mensa, suomi, finland, älykkyystestit, eurooppalainen älykkyystesti,
+psykologinen testi, sosiaalinen älykkyys, älypeli, älypää, psykologinen, logiikka, looginen, loogiset, päättely, kyky, tehtäviä, tunneäly, ilmainen nettitesti, matemaattinen, numeerinen, numeeriset, raja, jakauma, online, järjestö, yhdistys, yhteisö, ryhmä, seura, seuraa, verkosto, vertais, tapahtuma, tapahtumia, kokemuksia, taito, lahjakkuus,
+lahjakas, lapsi, tuki, erilaisuus, perspektiivi, harrastus, ystävä, ystäviä, kavereita, keskustelu, sananvapaus, chat, huumori, pulmapeli, pulmapelit, vapaaehtois, hyväntekeväisyys, parantaa, sukututkimus, tutustuminen, tekemistä, kohtaamisia, yhteistyö, oppiminen, oppia, mahdollisuus, kansainvälinen, maailmanlaajuinen, toiminta, toimintaa, älyä, oivallusta, oivaltaminen, oivaltaa, &auml;lykkyys, &auml:o, &auml;lykas, alykkyys, aly, alykas</h1>
 
 		<div id="MensaLogo"><a href="index.php"><img src="<?php bloginfo('template_directory'); ?>/images/MensaLogo.png" alt="Mensa Logo" /></a></div><!-- end MensaLogo -->
 		<div id="MensaText"><a href="index.php"><img src="<?php bloginfo('template_directory'); ?>/images/MensaText.png" alt="Mensa Finland" /></a></div><!-- end MensaText -->
